83a48b2c8952ff3f26e5ab648e326a981f1726b9
Ayarza Manwaring <ayarza.manwaring@gmail.com>
2019-07-30 11:53:23 -0400

    front end all products view

---
+++ client/components/allProducts.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import {productsThunk} from '../store/product'
+
+class AllProducts extends React.Component {
+  componentDidMount() {
+    this.props.productsThunk()
+  }
+  render() {
+    const {products} = this.props
+    return (
+      <div>
+        {products.map(product => {
+          return (
+            <div key={product.id}>
+              <img src={product.imgUrl} />
+              <h2>Name: {product.name}</h2>
+              <p>Price: {product.price}</p>
+              <p>Location: {product.location}</p>
+              <p>Description: {product.description}</p>
+            </div>
+          )
+        })}
+      </div>
+    )
+  }
+}
+const mapStateToProps = state => ({
+  products: state.productList
+})
+
+const mapDispatchToProps = dispatch => ({
+  productsThunk: () => dispatch(productsThunk)
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(AllProducts)
---
+++ client/store/product.js
@@ -0,0 +1,55 @@
+import axios from 'axios'
+
+//Action types
+const GET_PRODUCTS = 'GET_PRODUCTS'
+const SINGLE_PRODUCT = 'SINGLE_PRODUCT'
+
+//action creators
+
+const getProducts = data => ({
+  type: GET_PRODUCTS,
+  data
+})
+
+const singleProduct = product => ({
+  type: SINGLE_PRODUCT,
+  product
+})
+
+const initialState = {
+  productList: [],
+  singleProduct: {}
+}
+
+//thunk
+
+export const productsThunk = () => async dispatch => {
+  try {
+    const {data} = await axios.get('/api/products')
+    dispatch(getProducts(data))
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+export const productThunk = id => async dispatch => {
+  try {
+    const {data} = await axios.get(`/api/products/${id}`)
+    dispatch(singleProduct(data))
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+//reducer
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case GET_PRODUCTS:
+      return {...state, productList: action.data}
+    case SINGLE_PRODUCT:
+      return {...state, singleProduct: action.product}
+    default:
+      return state
+  }
+}
