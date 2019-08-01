b23aae5de73807b5d3c8edf330b8f5f328a12fd2
Yuting Zhang <yutingzhang729@gmail.com>
2019-07-31 20:30:57 -0400

    checkout works, some rendering fixed on frontend for cart/checkout

--- client/routes.js
+++ client/routes.js
@@ -29,7 +29,7 @@ class Routes extends Component {
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
         <Route exact path="/products" component={AllProducts} />
-        <Route exact path="/cart" component={Cart} />
+        <Route exact path="/cart" render={props => <Cart {...props} />} />
         <Route exact path="/checkout" component={Checkout} />
         <Route
           path="/products/:id"
