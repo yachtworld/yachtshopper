60ea510933e832bcc0c0bb739ff8d89f66a24a76
Yuting Zhang <yutingzhang729@gmail.com>
2019-08-01 16:32:54 -0400

    single item checkout, fixed product route in express

--- server/api/products.js
+++ server/api/products.js
@@ -4,7 +4,7 @@ module.exports = router
 
 router.get('/', async (req, res, next) => {
   try {
-    const products = await Products.findAll()
+    const products = await Products.findAll({order: [['id', 'ASC']]})
     res.json(products)
   } catch (err) {
     next(err)
