9c910a8ec7a425657144bef553eb3315d259835f
Monica Zhou <zhoumonu@gmail.com>
2019-08-05 17:44:36 -0400

    security no middleware

--- server/api/cart.js
+++ server/api/cart.js
@@ -20,8 +20,9 @@ router.get('/', async (req, res, next) => {
       const user = await User.findByPk(req.session.passport.user)
       const userCart = await user.getProducts()
       res.json(userCart.map(item => item.id))
+    } else {
+      res.status(500).send([])
     }
-    res.status(500).send([])
   } catch (error) {
     res.send([])
   }
@@ -29,11 +30,15 @@ router.get('/', async (req, res, next) => {
 
 router.put('/add', async (req, res, next) => {
   try {
-    const user = await User.findByPk(req.user.id)
-    const product = await Products.findByPk(req.body.id)
-    user.addProduct(product)
-    const allProducts = await user.getProducts()
-    res.json(allProducts.map(item => item.id)).sendStatus(200)
+    if (req.user.id === req.session.passport.user) {
+      const user = await User.findByPk(req.user.id)
+      const product = await Products.findByPk(req.body.id)
+      user.addProduct(product)
+      const allProducts = await user.getProducts()
+      res.json(allProducts.map(item => item.id)).sendStatus(200)
+    } else {
+      res.status(500).send([])
+    }
   } catch (error) {
     res.send({error: 'cart not found'})
   }
@@ -41,29 +46,33 @@ router.put('/add', async (req, res, next) => {
 
 router.put('/checkout', async (req, res, next) => {
   try {
-    const user = await User.findByPk(req.user.id)
-    const order = await user.getProducts()
-    user.setProducts([])
-    let userOrders = await Order.findAll({where: {userId: req.user.id}})
-    let orderId = Math.max(
-      ...userOrders.map(product => parseInt(product.orderId, 10))
-    )
+    if (req.user.id === req.session.passport.user) {
+      const user = await User.findByPk(req.user.id)
+      const order = await user.getProducts()
+      user.setProducts([])
+      let userOrders = await Order.findAll({where: {userId: req.user.id}})
+      let orderId = Math.max(
+        ...userOrders.map(product => parseInt(product.orderId, 10))
+      )
 
-    if (orderId && isFinite(orderId)) {
-      orderId += 1
-    } else {
-      orderId = 1
-    }
-    order.forEach(async item => {
-      await Order.create({
-        userId: user.id,
-        productId: item.id,
-        orderId: orderId,
-        productName: item.name
+      if (orderId && isFinite(orderId)) {
+        orderId += 1
+      } else {
+        orderId = 1
+      }
+      order.forEach(async item => {
+        await Order.create({
+          userId: user.id,
+          productId: item.id,
+          orderId: orderId,
+          productName: item.name
+        })
+        await Products.update({sold: true}, {where: {id: item.id}})
       })
-      await Products.update({sold: true}, {where: {id: item.id}})
-    })
-    res.json(order.map(product => product.id)).sendStatus(200)
+      res.json(order.map(product => product.id)).sendStatus(200)
+    } else {
+      res.status(500).send([])
+    }
   } catch (error) {
     res.send({error: 'cart not found'})
   }
@@ -71,11 +80,15 @@ router.put('/checkout', async (req, res, next) => {
 
 router.put('/delete', async (req, res, next) => {
   try {
-    const user = await User.findByPk(req.user.id)
-    const product = await Products.findByPk(req.body.id)
-    await user.removeProduct(product)
-    const newCart = await user.getProducts()
-    res.json(newCart.map(item => item.id)).sendStatus(200)
+    if (req.user.id === req.session.passport.user) {
+      const user = await User.findByPk(req.user.id)
+      const product = await Products.findByPk(req.body.id)
+      await user.removeProduct(product)
+      const newCart = await user.getProducts()
+      res.json(newCart.map(item => item.id)).sendStatus(200)
+    } else {
+      res.status(500).send([])
+    }
   } catch (error) {
     res.send({error: 'cart not found'})
   }
