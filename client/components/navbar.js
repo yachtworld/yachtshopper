d1de9d4c446ee3579a6ee58dc2d1a3a7b619a666
Yuting Zhang <yutingzhang729@gmail.com>
2019-08-06 16:24:30 -0400

    navbar split into right and left

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -8,46 +8,57 @@ import {LinkContainer} from 'react-router-bootstrap'
 
 const Navbar = ({handleClick, isLoggedIn, isAdmin}) => (
   <div>
-    <NavbarReact bg="light" variant="light">
-      <NavbarReact.Brand>Island World</NavbarReact.Brand>
-      {isLoggedIn ? (
-        <React.Fragment>
-          {/* The navbar will show these links after you log in */}
-          <LinkContainer to="/home">
-            <Nav.Link>Home</Nav.Link>
-          </LinkContainer>
-          <LinkContainer to="/#">
-            <Nav.Link onClick={handleClick}>Logout</Nav.Link>
-          </LinkContainer>
-        </React.Fragment>
-      ) : (
-        <React.Fragment>
-          {/* The navbar will show these links before you log in */}
-          <LinkContainer to="/login">
-            <Nav.Link>Login</Nav.Link>
-          </LinkContainer>
-          <LinkContainer to="/signup">
-            <Nav.Link>Sign Up</Nav.Link>
-          </LinkContainer>
-        </React.Fragment>
-      )}
-      <LinkContainer to="/products">
-        <Nav.Link>All Products</Nav.Link>
-      </LinkContainer>
-      <LinkContainer to="/cart">
-        <Nav.Link>Cart</Nav.Link>
-      </LinkContainer>
+    <NavbarReact bg="light" variant="light" id="navbar-all">
+      <div id="navbar-left">
+        <NavbarReact.Brand href="/">Island World</NavbarReact.Brand>
+        {isLoggedIn ? (
+          <React.Fragment>
+            {/* The navbar will show these links after you log in */}
+            <LinkContainer to="/home">
+              <Nav.Link>My Profile</Nav.Link>
+            </LinkContainer>
+          </React.Fragment>
+        ) : (
+          <div />
+        )}
+        <LinkContainer to="/products">
+          <Nav.Link>All Products</Nav.Link>
+        </LinkContainer>
+      </div>
+      <div id="navbar-right">
+        {isAdmin ? (
+          <React.Fragment>
+            {/* The navbar will show these links after you log in */}
+            <LinkContainer to="/admin">
+              <Nav.Link>Admin Panel</Nav.Link>
+            </LinkContainer>
+          </React.Fragment>
+        ) : (
+          <React.Fragment />
+        )}
+        <LinkContainer to="/cart">
+          <Nav.Link>Cart</Nav.Link>
+        </LinkContainer>
+        {isLoggedIn ? (
+          <React.Fragment>
+            {/* The navbar will show these links after you log in */}
 
-      {isAdmin ? (
-        <React.Fragment>
-          {/* The navbar will show these links after you log in */}
-          <LinkContainer to="/admin">
-            <Nav.Link>Admin Panel</Nav.Link>
-          </LinkContainer>
-        </React.Fragment>
-      ) : (
-        <React.Fragment />
-      )}
+            <LinkContainer to="/#">
+              <Nav.Link onClick={handleClick}>Logout</Nav.Link>
+            </LinkContainer>
+          </React.Fragment>
+        ) : (
+          <React.Fragment>
+            {/* The navbar will show these links before you log in */}
+            <LinkContainer to="/login">
+              <Nav.Link>Login</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/signup">
+              <Nav.Link>Sign Up</Nav.Link>
+            </LinkContainer>
+          </React.Fragment>
+        )}
+      </div>
     </NavbarReact>
     <hr />
   </div>
